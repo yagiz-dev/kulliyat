b5ff3f479009728bfbbbde0b2c17313601da97c8
evrifaessa <hi@yagiz.dev>
2026-08-08 21:37:43 +0300

    Initialize Spring Boot project

---
+++ src/main/java/dev/yagiz/kulliyat/KulliyatApplication.java
@@ -0,0 +1,13 @@
+package dev.yagiz.kulliyat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulliyatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KulliyatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yagiz/kulliyat/KulliyatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yagiz.kulliyat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KulliyatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
